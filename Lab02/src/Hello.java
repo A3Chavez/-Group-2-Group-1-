19961943fe6e94814b68466e8ea813fcdc9912b4
Hoodie <34972840+Mushuooshu@users.noreply.github.com>
2021-03-10 15:41:36 -0800

    Updated line 8 with my information

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -15,7 +15,6 @@ public class Hello {
 		System.out.println("219855936 - This line was added by Anthony Chavez.");
 		System.out.println("219855936 - This line was added by Anthony Chavez.");
 		System.out.println("219855936 - This line was added by Anthony Chavez.");
-
 	}
 
 }
