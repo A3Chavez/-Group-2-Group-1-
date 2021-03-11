c79d0526e34cf5045805a9dac50c8f828690736c
Ahmed <Ahmed@DESKTOP-10AQVAL>
2021-03-10 16:21:30 -0800

    Added my name - Ahmed

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,13 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("Chavez - This line was added by Anthony Chavez");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
-		System.out.println("219855936 - This line was added by Anthony Chavez.");
+		System.out.println("301362139 - This line was added by Muhammad Ahmed.");
 		System.out.println("219855936 - This line was added by Anthony Chavez.");
 		System.out.println("219855936 - This line was added by Anthony Chavez.");
 		
